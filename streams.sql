49f5ec0e94fa9fb2af4c8b186cd7fc674de1ea54
itu-itis24-ozdemirme23 <ozdemirme23@itu.edu.tr>
2024-10-18 10:25:55 +0300

    edited

--- streams.sql
+++
@@ -1,9 +0,0 @@
-SELECT TOP 5 
-s.Track, s.Artist,
-s.[Spotify Streams], 
-s.[YouTube Views]
-
-FROM streamedSongs_Table s
-JOIN songText_Table t ON s.Artist = t.artist AND s.Track = t.song
-WHERE t.text LIKE '%fuck%'
-ORDER BY s.[Spotify Streams] DESC;
